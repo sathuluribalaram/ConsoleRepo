0b121e0f50f100fbf2392b92acdfe35d98452898
balaramsathuluri <s.pavan1249@gmail.com>
2018-01-11 14:31:42 +0530

    Added CommonFunctions Project

---
+++ CommonFunctions/Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonFunctions
+{
+    public class Log
+    {
+        public static void Error()
+        {
+            Console.WriteLine("Error Occured");
+        }
+
+        public static void Debug(string debugMessage)
+        {
+            Console.WriteLine(debugMessage);
+        }
+    }
+}
