4a58424997d18a023e9bd4e826bb91038a2ce272
balaramsathuluri <s.pavan1249@gmail.com>
2018-01-22 20:59:13 +0530

    Demo for Var.Dynamic,Out,Ref keywords

---
+++ CommonFunctions/VarDynamicOutRef.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonFunctions
+{
+    public class VarDynamicOutRef
+    {
+
+        public void Varusage()
+        {
+            Console.WriteLine("Enter a value");
+            var value =int.Parse(Console.ReadLine());
+            Console.WriteLine("value is of type" + value.GetType());
+        }
+        public void DynamicUsage()
+        {
+            dynamic name = 123;
+            string namestring=name.ToString();
+            Console.WriteLine(namestring);
+        }
+        public void Add(ref int a, int b)
+        {
+            a = a + b;
+            Console.WriteLine("value of a after executing method" + a);
+        }
+        public void Add(ref int a, int b, out int total, out int product)
+        {
+            Console.WriteLine("value of a is" + a);
+            total = a + b;
+            product = a * b;
+
+        }
+    }
+}
--- InterfaceDemo/Program.cs
+++ InterfaceDemo/Program.cs
@@ -32,11 +32,22 @@ namespace InterfaceDemo
             //Console.WriteLine("Enter Main String");
             //string mainstring = Console.ReadLine();
 
-            StringOperations stringoperationsobj = new StringOperations();
-            string substring=stringoperationsobj.ReadSubstring();
-            //string output=stringoperationsobj.Issubstring(mainstring,substring);
-            //Console.WriteLine(output);
-            Console.WriteLine("reverse of the string is " + stringoperationsobj.ReverseString(substring));
+            //StringOperations stringoperationsobj = new StringOperations();
+            //string substring=stringoperationsobj.ReadSubstring();
+            ////string output=stringoperationsobj.Issubstring(mainstring,substring);
+            ////Console.WriteLine(output);
+            //Console.WriteLine("reverse of the string is " + stringoperationsobj.ReverseString(substring));
+            int a=10;
+            int sum;
+            int multiply;
+            VarDynamicOutRef varDynamicOutRefObj = new VarDynamicOutRef();
+            varDynamicOutRefObj.Varusage();
+            varDynamicOutRefObj.DynamicUsage();
+            Console.WriteLine("value of a before executing method" + a);
+            varDynamicOutRefObj.Add(ref a, 20);
+            Console.WriteLine("value of a after executing method" + a);
+            varDynamicOutRefObj.Add(ref a, 30, out sum, out multiply);
+            Console.WriteLine("values of sum is {0} and multiply is {1}", sum, multiply);
             Console.ReadLine();
         }
             
